50d2e9e4ca3f93f39fdd9de93390257b16491c85
mightyfrog <mightyfrog.gc@gmail.com>
2019-02-03 04:04:37 +0900

    lib update + cleanup

--- app/src/main/java/org/mightyfrog/android/imagedecodersample/MainActivity.kt
+++ app/src/main/java/org/mightyfrog/android/imagedecodersample/MainActivity.kt
@@ -6,16 +6,19 @@ import android.graphics.ImageDecoder
 import android.graphics.drawable.Animatable2
 import android.net.Uri
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 /**
  * @author Shigehiro Soejima
  */
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    private val job = Job()
+
+    override val coroutineContext: CoroutineContext get() = Dispatchers.Main + job
 
     @SuppressLint("LogNotTimber")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,23 +28,13 @@ class MainActivity : AppCompatActivity() {
 
         val listener = ImageDecoder.OnHeaderDecodedListener { _, info, _ -> android.util.Log.e("ImageDecoder", "$info") }
         val src = ImageDecoder.createSource(contentResolver, drawableToUri(R.drawable.earth))
-        ImageDecoder.decodeDrawable(src, listener).apply {
-            imageView.setImageDrawable(this)
-            if (this is Animatable2) {
-                start()
+        launch {
+            withContext(Dispatchers.IO) { ImageDecoder.decodeDrawable(src, listener) }.apply {
+                imageView.setImageDrawable(this)
+                if (this is Animatable2) {
+                    start()
+                }
             }
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
         }
     }
 
