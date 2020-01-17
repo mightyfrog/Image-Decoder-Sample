73c0a2e1c1ec1108f518d7541670c08467d3a5a8
mightyfrog <mightyfrog.gc@gmail.com>
2020-01-18 05:30:50 +0900

    lib update

--- app/src/main/java/org/mightyfrog/android/imagedecodersample/MainActivity.kt
+++ app/src/main/java/org/mightyfrog/android/imagedecodersample/MainActivity.kt
@@ -7,18 +7,19 @@ import android.graphics.drawable.Animatable2
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
-import kotlin.coroutines.CoroutineContext
+import kotlinx.android.synthetic.main.activity_main.imageView
+import kotlinx.android.synthetic.main.activity_main.toolbar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * @author Shigehiro Soejima
  */
-class MainActivity : AppCompatActivity(), CoroutineScope {
-
-    private val job = Job()
-
-    override val coroutineContext: CoroutineContext get() = Dispatchers.Main + job
+class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
 
     @SuppressLint("LogNotTimber")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +27,12 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        val listener = ImageDecoder.OnHeaderDecodedListener { _, info, _ -> android.util.Log.e("ImageDecoder", "$info") }
+        val listener = ImageDecoder.OnHeaderDecodedListener { _, info, _ ->
+            android.util.Log.e(
+                "ImageDecoder",
+                "$info"
+            )
+        }
         val src = ImageDecoder.createSource(contentResolver, drawableToUri(R.drawable.earth))
         launch {
             withContext(Dispatchers.IO) { ImageDecoder.decodeDrawable(src, listener) }.apply {
@@ -38,7 +44,17 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+
+        cancel()
+    }
+
     private fun drawableToUri(resId: Int): Uri {
-        return Uri.parse("${ContentResolver.SCHEME_ANDROID_RESOURCE}://${resources.getResourcePackageName(resId)}/${resources.getResourceTypeName(resId)}/${resources.getResourceEntryName(resId)}")
+        return Uri.parse(
+            "${ContentResolver.SCHEME_ANDROID_RESOURCE}://${resources.getResourcePackageName(
+                resId
+            )}/${resources.getResourceTypeName(resId)}/${resources.getResourceEntryName(resId)}"
+        )
     }
 }
